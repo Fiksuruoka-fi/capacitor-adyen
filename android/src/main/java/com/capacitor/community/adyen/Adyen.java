ba2a6e381c6a65d6c1b0d530c978875e2988508a
Eljas <eljas.jalo@fiksuruoka.fi>
2025-09-22 16:45:28 +0300

    Initial commit

---
+++ android/src/main/java/com/capacitor/community/adyen/Adyen.java
@@ -0,0 +1,11 @@
+package com.capacitor.community.adyen;
+
+import com.getcapacitor.Logger;
+
+public class Adyen {
+
+    public String echo(String value) {
+        Logger.info("Echo", value);
+        return value;
+    }
+}
