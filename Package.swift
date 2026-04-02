72cb472828b67ab381d7adaab4e1fb6130e0b83e
Eljas <eljas.jalo@fiksuruoka.fi>
2026-04-02 11:26:44 +0300

    chore: bump default adyen ios sdk from v5.22.1 to v5.23.1

--- Package.swift
+++ Package.swift
@@ -2,31 +2,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "CapacitorAdyen",
-    platforms: [.iOS(.v15)],
-    products: [
-        .library(
-            name: "CapacitorAdyen",
-            targets: ["AdyenPlugin"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
-        .package(url: "https://github.com/Adyen/adyen-ios.git", .upToNextMajor(from: "5.20.1"))
-    ],
-    targets: [
-        .target(
-            name: "AdyenPlugin",
-            dependencies: [
-                .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm"),
-                .product(name: "Adyen", package: "adyen-ios"),
-                .product(name: "AdyenComponents", package: "adyen-ios"),
-                .product(name: "AdyenCard", package: "adyen-ios")
-            ],
-            path: "ios/Sources/AdyenPlugin"),
-        .testTarget(
-            name: "AdyenPluginTests",
-            dependencies: ["AdyenPlugin"],
-            path: "ios/Tests/AdyenPluginTests")
-    ]
+  name: "CapacitorAdyen",
+  platforms: [.iOS(.v15)],
+  products: [
+    .library(
+      name: "CapacitorAdyen",
+      targets: ["AdyenPlugin"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
+    .package(url: "https://github.com/Adyen/adyen-ios.git", .upToNextMajor(from: "5.23.1")),
+  ],
+  targets: [
+    .target(
+      name: "AdyenPlugin",
+      dependencies: [
+        .product(name: "Capacitor", package: "capacitor-swift-pm"),
+        .product(name: "Cordova", package: "capacitor-swift-pm"),
+        .product(name: "Adyen", package: "adyen-ios"),
+        .product(name: "AdyenComponents", package: "adyen-ios"),
+        .product(name: "AdyenCard", package: "adyen-ios"),
+      ],
+      path: "ios/Sources/AdyenPlugin"),
+    .testTarget(
+      name: "AdyenPluginTests",
+      dependencies: ["AdyenPlugin"],
+      path: "ios/Tests/AdyenPluginTests"),
+  ]
 )
